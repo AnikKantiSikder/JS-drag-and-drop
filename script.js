4983332ad76afe239b616b2b695458d4f9cd080c
Anik <sikder35-1408@diu.edu.bd>
2022-05-30 20:41:41 +0600

    Project complete

--- script.js
+++ script.js
@@ -1,46 +1,47 @@
 
-const body = document.body;
-const slides = document.querySelectorAll('.slide');
+const fill = document.querySelector('.fill');
+const emptyBoxes = document.querySelectorAll('.empty');
 
-const lefBtn = document.getElementById('left');
-const rightBtn = document.getElementById('right');
+fill.addEventListener('dragstart', dragStart);
+fill.addEventListener('dragend', dragEnd);
 
-let activeSlide = 0;
-
-rightBtn.addEventListener('click', () => {
-    activeSlide++;
-
-    if(activeSlide > slides.length-1) {
-        activeSlide = 0;
-    }
-
-    setBgToBody();
-    setActiveSlide();
-});
-
-lefBtn.addEventListener('click', () => {
-    activeSlide--;
-
-    if(activeSlide < 0) {
-        activeSlide = slides.length-1;
-    }
-
-    setBgToBody();
-    setActiveSlide();
-})
-
-
-setBgToBody();
-
-function setBgToBody(){
-    body.style.backgroundImage = 
-    slides[activeSlide].style.backgroundImage;
+// For the empty boxes
+for (const empty of emptyBoxes) {
+    empty.addEventListener('dragover', dragOver);
+    empty.addEventListener('dragenter', dragEnter);
+    empty.addEventListener('dragleave', dragLeave);
+    empty.addEventListener('drop', dragDrop);
 }
 
-function setActiveSlide(){
-    slides.forEach((slide) => {
-        slide.classList.remove('active');
-    });
+function dragStart() {
+    //console.log("dragStart");
+    this.className += ' hold';
+    setTimeout(() =>  this.className = 'invisible', 0);
+}
 
-    slides[activeSlide].classList.add('active');
+function dragEnd() {
+    // console.log("dragEnd");
+    this.className = 'fill';
+}
+
+function dragOver(e) {
+    //console.log("dragOver");
+    e.preventDefault();
+}
+
+function dragEnter(e) {
+    //console.log("dragEnter");
+    e.preventDefault();
+    this.className += ' hovered';
+}
+
+function dragLeave() {
+    //console.log("dragLeave");
+    this.className = 'empty';
+}
+
+function dragDrop() {
+    //console.log("dragDrop");
+    this.className = 'empty';
+    this.append(fill);
 }
